3c11927316e31de1bcdf434a6ea744af29a02338
edvinbasil <edvinbasil@gmail.com>
2018-09-07 02:32:07 +0530

    Added nodemod,morgan and error handling Overview of changes: Nodemon: provides auroreload server on changes, changed server.js to start nodemon on init Morgan: provides more logs on the incoming requests. routed morgan accordingly Error handling functions defined in app.js They now handle both errors from routes like invalid methods or non existent paths, and also other internal errors returned by the server.

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-app.use('/products', productRoutes); 		//Sets up middelware
+//Sets up middelware
+app.use(morgan('dev'));
+app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//Error Handling for routes
+app.use((req,res,next) => {
+    const error = new Error('Not Found');
+    error.status = 400;
+    next(error);
+});
+
+//Error Handling for other internal errors
+app.use((error,req,res,next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
